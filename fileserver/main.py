dfc0873e661937793257e83ff024f0f36f204476
Wind2009-Louse <wind.jackey@qq.com>
2019-04-03 10:45:01 +0800

    REAL multithread

--- fileserver/main.py
+++ fileserver/main.py
@@ -1,15 +1,17 @@
-from twisted.web import xmlrpc, server
 import json
 import os
 import hashlib
 import threading
 import time
+from socketserver import ThreadingMixIn
+from xmlrpc.server import SimpleXMLRPCServer
 
 nearby_servers = []
 timeout_times = {}
 fileversion = {}
 version_filename = os.path.join(os.getcwd(),"dir.txt")
 Lock = threading.Lock()
+FLock = {}
 TIMEOUT_TIME = 3
 
 def dir_md5(dir):
@@ -30,6 +32,17 @@ def update_version():
     new_file = open(filename, 'w')
     new_file.write(write_json)
     new_file.close()
+
+# file lock
+def acquire_flock(fname):
+    if fname not in FLock:
+        FLock[fname] = threading.Lock()
+    FLock[fname].acquire()
+def release_flock(fname):
+    if fname not in FLock:
+        FLock[fname] = threading.Lock()
+        return
+    FLock[fname].release()
 
 # connect to main server
 def connect_to_server(self_port):
@@ -128,160 +141,181 @@ def send_json(port):
     srv.updatelist(json.dumps(fileversion))
 
 # RPC
-class filesystem(xmlrpc.XMLRPC):
-    # return 0
-    # call by other file server
-    def xmlrpc_addnearby(self, port):
-        Lock.acquire()
-        # dump connect
-        if port not in nearby_servers:
-            nearby_servers.append(port)
-        send_json(port)
+
+class ThreadXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
+    pass
+
+# return 0
+# call by other file server
+def xmlrpc_addnearby(port):
+    Lock.acquire()
+    # dump connect
+    if port not in nearby_servers:
+        nearby_servers.append(port)
+    send_json(port)
+    Lock.release()
+    return 0
+# return 0
+# call by other file server
+def xmlrpc_updatelist(versionjson):
+    global fileversion
+    global self_port
+    tocheck_data = json.loads(versionjson)
+    for fullname in fileversion:
+        file_localname = os.path.join(os.getcwd(), "files_%d"%self_port, dir_md5(fullname))
+        if fullname not in tocheck_data\
+                or (fileversion[fullname] > tocheck_data[fullname] and tocheck_data[fullname] != -1)\
+                or (fileversion[fullname] == -1 and tocheck_data[fullname] != -1):
+            if fileversion[fullname] == -1:
+                data = False
+            else:
+                acquire_flock(fullname)
+                open_file = open(file_localname, 'rb')
+                data = open_file.read()  # data: bytes
+                open_file.close()
+                release_flock(fullname)
+            broadcast_file(fileversion[fullname], fullname, data)
+    return 0
+# return 0
+# call by server
+def xmlrpc_findfile(full_filename):
+    global fileversion
+    Lock.acquire()
+    file_localname = os.path.join(os.getcwd(), "files_%d"%self_port, dir_md5(full_filename))
+    if full_filename in fileversion \
+        and not os.path.exists(file_localname):
+        fileversion.pop(full_filename)
+    if full_filename not in fileversion:
         Lock.release()
-        return 0
-    # return 0
-    # call by other file server
-    def xmlrpc_updatelist(self, versionjson):
-        global fileversion
-        global self_port
-        tocheck_data = json.loads(versionjson)
-        for fullname in fileversion:
-            file_localname = os.path.join(os.getcwd(), "files_%d"%self_port, dir_md5(fullname))
-            if fullname not in tocheck_data\
-                    or (fileversion[fullname] > tocheck_data[fullname] and tocheck_data[fullname] != -1)\
-                    or (fileversion[fullname] == -1 and tocheck_data[fullname] != -1):
-                if fileversion[fullname] == -1:
-                    data = False
-                else:
-                    open_file = open(file_localname, 'rb')
-                    data = open_file.read()  # data: bytes
-                    open_file.close()
-                broadcast_file(fileversion[fullname], fullname, data)
-        return 0
-    # return 0
-    # call by server
-    def xmlrpc_findfile(self, full_filename):
-        global fileversion
-        Lock.acquire()
-        file_localname = os.path.join(os.getcwd(), "files_%d"%self_port, dir_md5(full_filename))
-        if full_filename in fileversion \
-            and not os.path.exists(file_localname):
-            fileversion.pop(full_filename)
-        if full_filename not in fileversion:
-            Lock.release()
-            return -1
-        version = fileversion[full_filename]
+        return -1
+    version = fileversion[full_filename]
+    Lock.release()
+    return version
+# return version, data
+# call by client
+def xmlrpc_readfile(full_filename):
+    global fileversion
+    Lock.acquire()
+    real_filename = file_realname(full_filename)
+    if not os.path.exists(real_filename) or full_filename not in fileversion:
         Lock.release()
-        return version
-    # return version, data
-    # call by client
-    def xmlrpc_readfile(self, full_filename):
-        global fileversion
-        Lock.acquire()
-        real_filename = file_realname(full_filename)
-        if not os.path.exists(real_filename) or full_filename not in fileversion:
-            Lock.release()
-            return -1, False
-        open_file = open(real_filename,'rb')
-        data = open_file.read() # data: bytes
-        open_file.close()
-        version = fileversion[full_filename]
+        return -1, False
+    Lock.release()
+
+    acquire_flock(full_filename)
+
+    if full_filename=="/test.txt":
+        while(1):
+            a = 1
+
+    open_file = open(real_filename,'rb')
+    data = open_file.read() # data: bytes
+    open_file.close()
+    release_flock(full_filename)
+
+    version = fileversion[full_filename]
+    return version, data
+# return 0(normal)
+# call by server
+def xmlrpc_readywrite(full_filename):
+    Lock.acquire()
+    if full_filename not in fileversion:
         Lock.release()
-        return version, data
-    # return 0(normal)
-    # call by server
-    def xmlrpc_readywrite(self, full_filename):
-        Lock.acquire()
-        if full_filename not in fileversion:
-            Lock.release()
-            return -1
-        fileversion[full_filename] += 1
-        update_version()
+        return -1
+    fileversion[full_filename] += 1
+    update_version()
+    Lock.release()
+    return 0
+# return 0(normal)
+# call by client
+def xmlrpc_writefile(version, full_filename, data):
+    Lock.acquire()
+    if full_filename in fileversion and fileversion[full_filename] > version:
         Lock.release()
-        return 0
-    # return 0(normal)
-    # call by client
-    def xmlrpc_writefile(self, version, full_filename, data):
-        Lock.acquire()
-        if full_filename in fileversion and fileversion[full_filename] > version:
-            Lock.release()
-            raise xmlrpc.Fault(403, "Your file is too old!")
-        real_name = file_realname(full_filename)
-        file = open(real_name,'wb')
-        file.write(data.data)
-        file.close()
-        fileversion[full_filename] = max(version, fileversion[full_filename]) + 1
-        new_version = fileversion[full_filename]
-        update_version()
+        raise Exception(403, "Your file is too old!")
+    real_name = file_realname(full_filename)
+    Lock.release()  
+
+    acquire_flock(full_filename)
+    file = open(real_name,'wb')
+    file.write(data.data)
+    file.close()
+    release_flock(full_filename)
+
+    Lock.acquire()
+    fileversion[full_filename] = max(version, fileversion[full_filename]) + 1
+    new_version = fileversion[full_filename]
+    update_version()
+    Lock.release()
+    broadcast_file(new_version, full_filename,data)
+    return 0
+# return 0(normal)
+# call by server
+def xmlrpc_createfile(full_filename):
+    Lock.acquire()
+    if full_filename in fileversion and fileversion[full_filename] != -1:
         Lock.release()
-        broadcast_file(new_version, full_filename,data)
-        return 0
-    # return 0(normal)
-    # call by server
-    def xmlrpc_createfile(self, full_filename):
-        Lock.acquire()
+        return -1
+    real_name = file_realname(full_filename)
+    # just create
+    acquire_flock(full_filename)
+    file = open(real_name, 'wb')
+    file.close()
+    release_flock(full_filename)
+    fileversion[full_filename] = 1
+    update_version()
+    Lock.release()
+    broadcast_file(1, full_filename, False)
+    return 0
+# return 0(normal)
+# call by server
+def xmlrpc_deletefile(full_filename):
+    Lock.acquire()
+    if full_filename not in fileversion:
+        Lock.release()
+        return -1
+    real_name = file_realname(full_filename)
+    # just delete
+    os.remove(real_name)
+    fileversion[full_filename] = -1
+    update_version()
+    Lock.release()
+    broadcast_file(-1, full_filename, False)
+    return 0
+# return 0(normal)
+# call by other file system
+def xmlrpc_updateremote(version, full_filename, data):
+    Lock.acquire()
+    real_name = file_realname(full_filename)
+    # delete
+    if version == -1:
         if full_filename in fileversion and fileversion[full_filename] != -1:
-            Lock.release()
-            return -1
-        real_name = file_realname(full_filename)
-        # just create
-        file = open(real_name, 'wb')
-        file.close()
-        fileversion[full_filename] = 1
-        update_version()
-        Lock.release()
-        broadcast_file(1, full_filename, False)
-        return 0
-    # return 0(normal)
-    # call by server
-    def xmlrpc_deletefile(self, full_filename):
-        Lock.acquire()
-        if full_filename not in fileversion:
-            Lock.release()
-            return -1
-        real_name = file_realname(full_filename)
-        # just delete
-        os.remove(real_name)
-        fileversion[full_filename] = -1
-        update_version()
-        Lock.release()
-        broadcast_file(-1, full_filename, False)
-        return 0
-    # return 0(normal)
-    # call by other file system
-    def xmlrpc_updateremote(self, version, full_filename, data):
-        Lock.acquire()
-        real_name = file_realname(full_filename)
-        # delete
-        if version == -1:
-            if full_filename in fileversion and fileversion[full_filename] != -1:
-                os.remove(real_name)
-                fileversion[full_filename] = -1
-                update_version()
-                Lock.release()
-                broadcast_file(-1, full_filename, False)
-                return 0
-            Lock.release()
-            return 0
-        # create
-        if full_filename not in fileversion\
-            or version > fileversion[full_filename]:
-            file = open(real_name, 'wb')
-            if data != False:
-                file.write(data.data)
-            file.close()
-            fileversion[full_filename] = version
+            os.remove(real_name)
+            fileversion[full_filename] = -1
             update_version()
             Lock.release()
-            broadcast_file(version, full_filename, data)
-        else:
-            Lock.release()
+            broadcast_file(-1, full_filename, False)
+            return 0
+        Lock.release()
         return 0
-    def xmlrpc_fault(self):
-        raise Exception("Unknown error.")
+    # create
+    if full_filename not in fileversion\
+        or version > fileversion[full_filename]:
+        file = open(real_name, 'wb')
+        if data != False:
+            file.write(data.data)
+        file.close()
+        fileversion[full_filename] = version
+        update_version()
+        Lock.release()
+        broadcast_file(version, full_filename, data)
+    else:
+        Lock.release()
+    return 0
+def xmlrpc_fault(self):
+    raise Exception("Unknown error.")
 
 if __name__ == '__main__':
-    from twisted.internet import reactor
 
     self_port = int(input("Input open port:"))
     connect_to_server(self_port)
@@ -298,6 +332,11 @@ if __name__ == '__main__':
         new_file = open(filename, 'w')
         new_file.write(json_data)
         new_file.close()
+    
+    # acquire a lock for each file
+    for k,v in fileversion.items():
+        if v != -1:
+            FLock[k] = threading.Lock()
 
     # timeout thread
     to_thread = timeout_thread()
@@ -308,6 +347,16 @@ if __name__ == '__main__':
     listen.start()
 
     # rpc system
-    srv = filesystem()
-    reactor.listenTCP(self_port, server.Site(srv))
-    reactor.run()
+    server = ThreadXMLRPCServer(('localhost', self_port)) # 初始化
+
+    server.register_function(xmlrpc_addnearby, "addnearby")
+    server.register_function(xmlrpc_updatelist, "updatelist")
+    server.register_function(xmlrpc_findfile, "findfile")
+    server.register_function(xmlrpc_readfile, "readfile")
+    server.register_function(xmlrpc_readywrite, "readywrite")
+    server.register_function(xmlrpc_writefile, "writefile")
+    server.register_function(xmlrpc_createfile, "createfile")
+    server.register_function(xmlrpc_deletefile, "deletefile")
+    server.register_function(xmlrpc_updateremote, "updateremote")
+
+    server.serve_forever() # 保持等待调用状态
